b37c6f8847ba0557d9096e6be3faed4413d610f6
Dare Ryan <dareryan@me.com>
2014-01-09 18:28:17 -0500

    Adding project files

---
+++ MedicationTracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MedicationTracker
+//
+//  Created by Dare Ryan on 1/9/14.
+//  Copyright (c) 2014 Dare Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
