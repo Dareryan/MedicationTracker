e1ef7573e8bacf74d1f40f9c719804fce42e10f6
Dare Ryan <dareryan@me.com>
2014-01-10 12:50:09 -0500

    Added web view with drug interaction checker

---
+++ MedicationTracker/WebViewController.h
@@ -0,0 +1,14 @@
+//
+//  WebViewController.h
+//  MedicationTracker
+//
+//  Created by Dare Ryan on 1/10/14.
+//  Copyright (c) 2014 Dare Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ MedicationTracker/WebViewController.m
@@ -0,0 +1,45 @@
+//
+//  WebViewController.m
+//  MedicationTracker
+//
+//  Created by Dare Ryan on 1/10/14.
+//  Copyright (c) 2014 Dare Ryan. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    NSString *fullURL = @"http://reference.medscape.com";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:requestObj];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
